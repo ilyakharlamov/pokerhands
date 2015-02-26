e8ec068e25025e753632d93ea80a7b1df7f7b3a2
Ilya Kharlamov <ilya.kharlamov@gmail.com>
2015-02-26 06:23:06 +0300

    Rm sources.txt

--- build.sh
+++ build.sh
@@ -2,4 +2,5 @@
 rm -f sources.txt
 find . -name "*.java" |grep -v Test> sources.txt
 javac @sources.txt
+rm -f sources.txt
 echo "now you can run as\njava -cp src com.ilyakharlamov.pockerhands.Standalone 2C3D4H5S6S 2D3D4D5D6D"
